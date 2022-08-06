9dfc42291b6118162c15a5dbdc6682913ebb438e
SaqibGhouse <saqib.ghouse12@gmail.com>
2022-08-06 01:17:33 -0700

    Side bar styled + bg image

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Sidebar.css"
 
 const SideBar = ({ children }) => {
   return <div className="SideBar">{children}</div>;
--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import "./User.css";
+import logo from "../../images/logo.png";
 const User = () => {
-  return <div className="User">User</div>;
+  return (
+    <div className="User">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="info">
+        <p>Saqib Ghouse</p>
+        <a href="#">Logout</a>
+      </div>
+    </div>
+  );
 };
 
 export default User;
