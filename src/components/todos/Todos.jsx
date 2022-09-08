79eeb491203ee2dd497761a5e83e2e975da785a0
SaqibGhouse <84792821+SaqibGhouse@users.noreply.github.com>
2022-09-08 17:36:40 +0500

    updates

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -1,31 +1,13 @@
 import React from "react";
+import { useContext } from "react";
+import { TodoContext } from "../../context";
 import Next7Days from "../next7Days/Next7Days";
 import Todo from "../todo/Todo";
 import "./Todos.css";
 
 const Todos = () => {
-  const todos = [
-    {
-      id: "d54sd4",
-      text: "Go for a run",
-      time: "10:00 AM",
-      date: "06/03/2021",
-      day: "6",
-      checked: false,
-      color: "#000000",
-      project: "personal",
-    },
-    {
-      id: "d54fdf",
-      text: "Meeting",
-      time: "09:00 AM",
-      date: "08/03/2021",
-      day: "1",
-      checked: true,
-      color: "#00ff00",
-      project: "work",
-    },
-  ];
+  const { todos } = useContext(TodoContext);
+
   return (
     <div className="Todos">
       <div className="todos">
---
+++ src/context/index.js
@@ -0,0 +1,39 @@
+import React, { createContext, useState } from "react";
+
+const TodoContext = createContext();
+const todos = [
+  {
+      id : 'd54sd4',
+      text : "Go for a run",
+      time : "10:00 AM",
+      date : "06/03/2021",
+      day : "6",
+      checked : false,
+      color : '#000000',
+      project : 'personal'
+  },
+  {
+      id : 'd54fdf',
+      text : "Meeting",
+      time : "09:00 AM",
+      date : "08/03/2021",
+      day : "1",
+      checked : true,
+      color : '#00ff00',
+      project : 'work'
+  }
+]
+function TodoContextProvider({ children }) {
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todos,
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+}
+
+export { TodoContextProvider, TodoContext };
